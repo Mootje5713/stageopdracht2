22e701b1fc96a1a481b85fcaefed5229e9edf9b7
Mohammed baallach <2113080@talnet.nl>
2023-04-24 19:06:19 +0200

    password hash is toegevoegd

--- login.php
+++ login.php
@@ -1,36 +1,41 @@
 <?php
-    include "connection.php";
-    if (isset($_POST['username']) && ($_POST['wachtwoord'])) {
-        $username =  $_POST['username'];
-        $password =  $_POST['wachtwoord'];
-        $query = "SELECT * FROM `user` WHERE username = '$username' 
-        AND wachtwoord = '$password'";
-        $result=$conn->query($query);
-        if ( $result === FALSE) {
-            echo "error" . $query . "<br />" . $conn->error;
-        } else {
-            if ($result->num_rows>0) {
-                while($row=$result->fetch_assoc())
-                {
-                    $_SESSION["user_id"]=$row['id'];
-                    $_SESSION["username"]=$row['username'];
+include "connection.php";
+if (isset($_POST['username']) && ($_POST['wachtwoord'])) {
+    $username =  $_POST['username'];
+    $password =  $_POST['wachtwoord'];
+    $query = "SELECT * FROM `user` WHERE username = '$username'";
+    $result = $conn->query($query);
+    if ($result === FALSE) {
+        echo "error" . $query . "<br />" . $conn->error;
+    } else {
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                if (password_verify($password, $row['wachtwoord'])) {
+                    $_SESSION["user_id"] = $row['id'];
+                    $_SESSION["username"] = $row['username'];
+                } else {
+                    echo "Wachtwoord niet gevonden!";
                 }
             }
+        } else {
+            echo "Gebruiker niet gevonden!";
         }
     }
-    if (isset($_SESSION['user_id'])) {
-        header('Location: index.php');
-    } 
-    $conn->close();
+}
+if (isset($_SESSION['user_id'])) {
+    header('Location: index.php');
+}
+$conn->close();
 ?>
 
-    <form method="POST">
-        username <input type="text" name="username" id="username" required>
-        <br>
-        password <input type="password" name="wachtwoord" id="wachtwoord" required>
-        <br>
-        <input type="submit" name="submit" value="sign in">
+<form method="POST">
+    username <input type="text" name="username" id="username" required>
+    <br>
+    password <input type="password" name="wachtwoord" id="wachtwoord" required>
+    <br>
+    <input type="submit" name="submit" value="sign in">
     nog geen account <a href="register.php">klik hier</a>
-    </form>
+</form>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,26 +1,28 @@
 <?php
-    include "connection.php";
-    if(isset($_POST['username']) && ($_POST['wachtwoord'])) {
-        $username =  $_POST['username'];
-        $password =  $_POST['wachtwoord'];
-        $user = "INSERT INTO `user`(username, wachtwoord)
+include "connection.php";
+if (isset($_POST['username']) && ($_POST['wachtwoord'])) {
+    $username =  $_POST['username'];
+    $password =  password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT);
+    $user = "INSERT INTO `user`(username, wachtwoord)
         VALUES ('$username', '$password')";
-        header("location: login.php");
-        if ( $conn->query($user) === FALSE) {
-            echo "error" . $user . "<br />" . $conn->error;
-        } else {
+    header("location: login.php");
+    if ($conn->query($user) === FALSE) {
+        echo "error" . $user . "<br />" . $conn->error;
+    } else {
     }
-    }
-    $conn->close();
+}
+$conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="POST">
         username <input type="text" name="username" id="username" required>
@@ -28,6 +30,7 @@
         password <input type="password" name="wachtwoord" id="wachtwoord" required>
         <br>
         <input type="submit" name="submit" value="sign up">
-        </form>
+    </form>
 </body>
+
 </html>
